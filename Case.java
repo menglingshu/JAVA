8c8bd24e44b1b1388eb87841211726e5cf513a85
MENG LINGSHU <mangomeng838@hotmail.com>
2018-01-27 22:19:56 +0100

    Case
    java exercise

---
+++ Case.java
@@ -0,0 +1,59 @@
+package pobj.motx.tme1;
+
+public class Case
+{
+	private int lig;
+	private int col;
+	private char c;
+	
+	public Case(int lig, int col, char c)
+	{
+		this.lig = lig;
+		this.col = col;
+		this.c = c;
+	}
+	
+	public int getLig() 
+	{
+		return lig;
+	}
+
+	public int getCol()
+	{
+		return col;
+	}
+
+	public char getChar() 
+	{
+		return c;
+	}
+
+	public void setChar(char c)
+	{
+		this.c = c;
+	}
+	
+	public boolean isVide()
+	{
+		if(this.getChar() == ' ')
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public boolean isPleine()
+	{
+		if(this.getChar() == '*')
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
